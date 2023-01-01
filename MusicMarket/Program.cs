66e1c91a4e2332e67823ebc439d758c3082e535a
kemalaydinn <kemal.aydin3429@gmail.com>
2023-01-01 22:10:37 +0300

    localize added

--- MusicMarket/Program.cs
+++ MusicMarket/Program.cs
@@ -3,8 +3,33 @@ using Microsoft.AspNetCore.Identity;
 using MusicMarket.Models;
 using Microsoft.Extensions.FileProviders;
 using MusicMarket.Data;
+using AspNetCoreMultilingual.Middlewares;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews()
+                .AddViewLocalization();
+
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new("en-US");
+
+    CultureInfo[] cultures = new CultureInfo[]
+    {
+        new("tr-TR"),
+        new("en-US"),
+    };
+
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+
+builder.Services.AddScoped<RequestLocalizationCookiesMiddleware>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataContext>(
@@ -38,7 +63,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
-
+app.UseRequestLocalization();
+app.UseRequestLocalizationCookies();
 app.UseRouting();
 app.UseAuthentication(); ;
 app.UseAuthorization();
